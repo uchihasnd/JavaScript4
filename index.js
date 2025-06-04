62b305b5de041d736cf69f5c7f211d05321d0809
uchihasnd <sandymiranda.snd@gmail.com>
2025-06-04 12:14:01 -0600

    States column added to the table, coordinate decimals reduced to 6

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function locationsTransformer(infoUniversity) {
   );
 
   const university = infoUniversity.features.map(
-    ({ attributes: { University_Chapter }, geometry: { x, y } }) => ({
+    ({ attributes: { University_Chapter, State }, geometry: { x, y } }) => ({
+      state: State,
       locationName: University_Chapter,
       latLng: [x, y],
     })
@@ -44,9 +45,10 @@ const tbody = document.querySelector(".universities-table tbody");
 universities.forEach((university) => {
   const row = document.createElement("tr");
   row.innerHTML = `
+    <td>${university.state}</td>
     <td class="university-title">${university.locationName}</td>
-    <td>${university.latLng[0]}</td>
-    <td>${university.latLng[1]}</td>
+    <td>${university.latLng[0].toFixed(6)}</td>
+    <td>${university.latLng[1].toFixed(6)}</td>
   `;
   tbody.appendChild(row);
 });
